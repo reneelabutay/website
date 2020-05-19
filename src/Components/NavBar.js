0f921af0f6d4abb01dabf37d2d0aa3e4125337d3
reneelabutay <reneelabutay@Renees-MacBook-Pro.local>
2020-05-19 15:07:08 -0700

    responsive nav-bar in progress

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,27 +1,66 @@
 import React, {Component} from 'react';
-import { slide as Menu } from 'react-burger-menu';
+import BurgerMenu from './BurgerMenu';
+import NavLinks from './NavLinks';
 
 export class NavBar extends Component {
-    showSettings (event) {
-        event.preventDefault();
+  constructor(props) {
+    super(props);
+    this.state = {
+      width: '', 
+      height: '', 
+      showBurger: false
     }
-    render () {
-      return (
-        <div className="header-bar">
-          <div className="title">
-            <p>Renee Labutay</p>
-          </div>
-          <div className="navigation-menu">
-            <Menu right>
-              <a className="home">Home</a>
-              <a className="about-me">About</a>
-              <a className="resume">Resume</a>
-              <a className="projects">Projects</a>
-            </Menu>
-          </div>
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+    this.burgerVisibility = this.burgerVisibility.bind(this);
+  }
+  showSettings (event) {
+      event.preventDefault();
+  }
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener('resize', this.updateWindowDimensions);
+	}
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions);
+  }
+  updateWindowDimensions() {
+    this.setState({ width: window.innerWidth, height: window.innerHeight });
+    this.burgerVisibility();
+  }
+
+  burgerVisibility() {
+    if(this.state.width < 650) {
+			this.setState({
+				showBurger: true
+			});
+		} else {
+			this.setState({
+				showBurger: false
+			})
+		}
+  }
+
+  render () {
+    const showBurger = this.state.showBurger;
+    let navlinks;
+    if (showBurger) {
+      console.log("burger is visible")
+      navlinks = <BurgerMenu/>
+    } else  {
+      navlinks = <NavLinks/>
+    }
+    
+    return (
+      <div className="header-bar">
+        <div className="title">
+          <p>Renee Labutay</p>
         </div>
-        
-      );
-    }
+        <div className="navigation-menu">
+          {navlinks}
+        </div>
+      </div>
+      
+    );
+  }
   }
   export default NavBar;
---
+++ src/Components/NavLinks.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function NavLinks() {
+    return (
+        <div>
+            <ul className="nav-links">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">FAQ</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </div>
+    );
+}
+export default NavLinks;
