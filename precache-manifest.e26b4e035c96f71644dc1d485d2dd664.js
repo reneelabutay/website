efb139392b0687ef20080dc87bcd7104efdda7b3
reneelabutay <reneelabutay@Renees-MacBook-Pro.local>
2020-06-19 20:17:21 -0700

    Updates

---
+++ precache-manifest.e26b4e035c96f71644dc1d485d2dd664.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b535778cfd46f80f49ca3b33737bf81a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "382c412e72ce32b15c52",
+    "url": "/static/css/main.b1471c20.chunk.css"
+  },
+  {
+    "revision": "a5a7e8ce8bacc20ef3d2",
+    "url": "/static/js/2.35c20d9f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.35c20d9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "382c412e72ce32b15c52",
+    "url": "/static/js/main.5588e1a8.chunk.js"
+  },
+  {
+    "revision": "2060b9a7e54a59ab7b07",
+    "url": "/static/js/runtime-main.f3a02c50.js"
+  },
+  {
+    "revision": "ec28e30545c5085e3aca3b17a529395d",
+    "url": "/static/media/self.ec28e305.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/website/precache-manifest.85be74ef7e3fe03e8cd0f8593fcd68ee.js"
+  "/precache-manifest.e26b4e035c96f71644dc1d485d2dd664.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
