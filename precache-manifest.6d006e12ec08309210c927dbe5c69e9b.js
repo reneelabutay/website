a7c42818b467289e6c44b2e93d76ad2c7265c7fb
reneelabutay <reneelabutay@Renees-MacBook-Pro.local>
2020-07-05 22:46:23 -0700

    Updates

---
+++ precache-manifest.6d006e12ec08309210c927dbe5c69e9b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f40d0045a0b91a38044c6130491e2d46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee7242cae182acc54567",
+    "url": "/static/css/main.06d96cd7.chunk.css"
+  },
+  {
+    "revision": "2b0e9bbde4c258ded7af",
+    "url": "/static/js/2.aeab545c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.aeab545c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee7242cae182acc54567",
+    "url": "/static/js/main.421231a4.chunk.js"
+  },
+  {
+    "revision": "2060b9a7e54a59ab7b07",
+    "url": "/static/js/runtime-main.f3a02c50.js"
+  },
+  {
+    "revision": "20fd82a038ed099e73808d860a60e5a9",
+    "url": "/static/media/logo.20fd82a0.png"
+  },
+  {
+    "revision": "ec28e30545c5085e3aca3b17a529395d",
+    "url": "/static/media/self.ec28e305.png"
+  }
+]);
